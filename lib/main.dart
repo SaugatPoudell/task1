7460c704ad9a64b6f76ee8c45ec0c23999e57688
SGT <977.saugat@gmail.com>
2024-05-14 10:05:34 +0545

    Homepage completed

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,8 @@
 // import 'dart:ffi';
 
+import 'dart:convert';
+
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 import 'package:geolocator/geolocator.dart';
@@ -24,7 +27,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home:MyHomePage()
     );
   }
 }
@@ -32,9 +35,9 @@ class MyApp extends StatelessWidget {
 class MyHomePage extends StatefulWidget {
 
 
-  const MyHomePage({super.key, required this.title});
+  const MyHomePage({super.key});
 
-  final String title;
+
   
 
   @override
@@ -42,15 +45,63 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+  bool apicalled=false;
  String textValue="";
+ Map<String ,dynamic>jsonData={};
+ 
+Future<Position>_getLocation() async {
+  Position position = await Geolocator.getCurrentPosition(
+    desiredAccuracy: LocationAccuracy.high,
+  );
+  print(position);
+  return position;
+}
+
+Future<void> getData() async
+{
+  Position position =await _getLocation();
+  print(position);
+  if(textValue.isEmpty)
+  {
+    final response = await http.get(Uri.parse("http://api.weatherapi.com/v1/current.json?key=1bc0383d81444b58b1432929200711&q=${position.latitude},${position.longitude}"));
+    jsonData =jsonDecode(response.body);
+  }
+  else
+  {
+    final response = await http.get(Uri.parse("http://api.weatherapi.com/v1/current.json?key=1bc0383d81444b58b1432929200711&q=$textValue"));
+    jsonData =jsonDecode(response.body);
+  }
+  // final response = await http.get(textValue.isEmpty? Uri.parse("http://api.weatherapi.com/v1/current.json?key=1bc0383d81444b58b1432929200711&q=${position.latitude},${position.longitude}"):Uri.parse("http://api.weatherapi.com/v1/current.json?key=1bc0383d81444b58b1432929200711&q=${textValue}"));
+
+// jsonData =jsonDecode(response.body);
+ if(jsonData.isNotEmpty)
+ {
+  setState(()
+  {
+    apicalled=true;
+  });
+print(jsonData['location']['name']);
+print(jsonData['current']['temp_c']);
+print(jsonData['current']['condition']['text']);
+print(jsonData['current']['condition']['icon']);
+
+ } 
+else
+{
+  print("Failed to load");
+}
+
+}
 //  double _currentLocation=0.0;
 //  double latitude=position.latitude;
  @override
   void initState() {
+    
     // TODO: implement initState
     super.initState();
-    _getLocation();
-    getData();
+     _getLocation();
+         getData();
+       
   }
 
 
@@ -64,39 +115,73 @@ class _MyHomePageState extends State<MyHomePage> {
       ),
       body: Center(
         child: Container(
-        
-          child: Column(children: [
-            // Row(
-            //   children: [
-            //     Text("Enter Location"),
-            //   ],
-            // ),
-            Row(
+          child: Column(mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+            Row(mainAxisAlignment: MainAxisAlignment.center,
               children: [
                 Expanded(
                   child: TextField(
+                    
                     onChanged: (value)
                     {
-                      setState(() {
+                      // getData();
+                      setState((){
                        textValue=value;
+                       print(textValue);
                       });
                     },
                     decoration: InputDecoration
                     (
+                      
                       hintText: "Enter your Location"
                     ),
                   ),
                 )
               ],
             ),
-            Row(
-              children: [
-                ElevatedButton(onPressed: (){}, child: Text
-                (
-                  textValue.isEmpty?"Save":"Update",
-                ))
+            Center(
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  ElevatedButton(onPressed: (){
+                    getData();
+                  }, child: Text
+                  (
+                    textValue.isEmpty?"Save":"Update",
+                  ))
+                ],
+              ),
+            ),
+            SizedBox(height: 10.0),
+            Column(
+              children: [!apicalled?
+                Text('Loading'):
+                Text(jsonData['location']['name']),
+                 Text('${jsonData['current']['temp_c']}°C'),
+                  Text(jsonData['current']['condition']['text']),
+                   Image.network(
+                    height: 100.0,
+                    width: 100.0,
+                    jsonData['current']['condition']['icon'])
               ],
             )
+//          ListTile(
+//   title: !apicalled ? Text('...Loading') : null,
+//   leading:!apicalled ? Image.network(jsonData['current']['condition']['icon']) : null,
+//   trailing:!apicalled ? Text('${jsonData['current']['temp_c']}') : null,
+//   subtitle: !apicalled ? Text(jsonData['current']['condition']['text']) : null,
+// ),
+           
+        
+      //  ListTile(
+      //   leading:Image.network(jsonData['current']['condition']['icon']),
+      //   trailing: Text(jsonData['current']['temp_c']),
+      //   title:Text(jsonData['location']['name']),
+      //   subtitle: Text(jsonData['current']['condition']['text']),
+      //  ),
+
+
         
             //  TextField(
             // decoration:InputDecoration
@@ -114,30 +199,6 @@ class _MyHomePageState extends State<MyHomePage> {
 
 
 
-Future<Position>_getLocation() async {
-  Position position = await Geolocator.getCurrentPosition(
-    desiredAccuracy: LocationAccuracy.high,
-  );
-  print(position);
-  return position;
-
-}
-
-Future<void> getData() async
-{
-  Position position =await _getLocation();
-  print(position);
-  final response = await http.get(Uri.parse("http://api.weatherapi.com/v1/current.json?key=1bc0383d81444b58b1432929200711&q=${position.latitude},${position.longitude}"));
- 
- if(response.statusCode==200)
- {
-print(response.body);
- } 
-else
-{
-  print("Failed to load");
-}
-}
 
 }
   // class HelpScreen extends StatefulWidget {
